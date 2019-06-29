c6aafd30e47ae21925a5d6028d484efa9d484ceb
wayofthepie <wayofthepie@gmail.com>
2019-06-29 23:09:48 +0100

    Add basic fragment

---
+++ common.js
@@ -0,0 +1,7 @@
+exports.react = require("react");
+exports["react-dom"] = require("react-dom");
+exports["react-redux"] = require("react-redux");
+exports["react-router"] = require("react-router");
+exports["react-router-dom"] = require("react-router-dom");
+exports["redux"] = require("redux");
+exports.classnames = require("classnames");
